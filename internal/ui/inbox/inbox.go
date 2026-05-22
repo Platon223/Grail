fc3ac447c99393be4379d604b3d8dc55974c3708
Platon <platon.tikhnenko@gmail.com>
2026-05-22 11:56:25 -0500

    added styling

--- internal/ui/inbox/inbox.go
+++ internal/ui/inbox/inbox.go
@@ -5,21 +5,35 @@ import (
 	//	"github.com/charmbracelet/bubbles/viewport"
 	"fmt"
 
-//	"github.com/charmbracelet/bubbles/viewport"
+	//	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	// "github.com/charmbracelet/glamour"
-	// "github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/glamour"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 
-type state int
+var (
+	boxStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#6e6a86")).
+			Padding(1, 2)
 
-const (
-	stateInbox state = iota
-	stateReading
+	titleStyle = lipgloss.NewStyle().
+			Background(lipgloss.Color("#31748f")).
+			Foreground(lipgloss.Color("230")).
+			Padding(0, 1).
+			Bold(true)
+
+	cursorStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#eb6f92")).
+			Bold(true)
 )
+
 
 type Email struct {
+	Header string
+	Name string
 	Body string
 	From string
 	To string
@@ -30,47 +44,69 @@ type InboxModel struct {
 	cursor int
 	viewport int
 	scroll int
+	item Email
+	renderer *glamour.TermRenderer
+	termWidth int
 }
 
 
 func NewInboxModel() InboxModel {
 
+	r, _ := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(60),
+	)
+
 	return InboxModel{
 		emails: []Email{
 			{
-				Body: "some email",
+				Header: "Header123",
+				Name: "Company",
+				Body: "some email 1",
+				From: "someuser@gmail.comhfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjfjfjfjfjfjfjjfjfjfjfjffjj",
+				To: "someuser@gmail.com",
+			},
+			{
+				Header: "Header123",
+				Name: "Company",
+				Body: "some email 2",
 				From: "someuser@gmail.com",
 				To: "someuser@gmail.com",
 			},
 			{
-				Body: "some email",
+				Header: "Header123",
+				Name: "Company",
+				Body: "some email 3",
 				From: "someuser@gmail.com",
 				To: "someuser@gmail.com",
 			},
 			{
-				Body: "some email",
+				Header: "Header123",
+				Name: "Company",
+				Body: "some email 4",
 				From: "someuser@gmail.com",
 				To: "someuser@gmail.com",
 			},
 			{
-				Body: "some email",
+				Header: "Header123",
+				Name: "Company",
+				Body: "some email 5",
 				From: "someuser@gmail.com",
 				To: "someuser@gmail.com",
 			},
 			{
-				Body: "some email",
-				From: "someuser@gmail.com",
-				To: "someuser@gmail.com",
-			},
-			{
-				Body: "some email",
+				Header: "Header123",
+				Name: "Company",
+				Body: "some email 6",
 				From: "someuser@gmail.com",
 				To: "someuser@gmail.com",
 			},
 		},
 		cursor: 0,
-		viewport: 5,
+		viewport: 10,
 		scroll: 0,
+		item: Email{},
+		renderer: r,
 	}
 }
 
@@ -82,8 +118,20 @@ func (m InboxModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	
 	switch msg := msg.(type) {
 
+	case tea.WindowSizeMsg:
+		m.termWidth = msg.Width
+
+		m.renderer, _ = glamour.NewTermRenderer(
+			glamour.WithAutoStyle(),
+			glamour.WithWordWrap(m.termWidth - 6),
+		)
+
 	case tea.KeyMsg:
 		switch msg.String() {
+
+		case "backspace", "esc":
+			m.item = Email{}
+
 
 		case "q", "ctrl+c":
 			return m, tea.Quit
@@ -104,6 +152,13 @@ func (m InboxModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.cursor >= m.scroll+m.viewport {
 				m.scroll = m.cursor - m.viewport + 1
 			}
+
+		case "enter":
+
+			if m.item == (Email{}) {
+				m.item = m.emails[m.cursor]	
+			}
+			
 		}
 	}
 
@@ -116,7 +171,15 @@ func (m InboxModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m InboxModel) View() string {
 
-	s := "Use up/down or j/k to scroll. Press q or ctrl+c to exit \n\n"
+	if m.item != (Email{}) {
+		s := fmt.Sprintf("j/k scroll - esc/backspace exit - r reply - o browser \n\nHeader: %s \nFrom: %s \n\nBody: %s", m.item.Header, m.item.From, m.item.Body)
+ 
+		title := titleStyle.Render(" Email View ")
+		box := boxStyle.Render(s)
+		return fmt.Sprintf("%s\n%s\n", title, box) 
+	}
+
+	s := "j/k scroll - enter pick - q/ctrl+c exit \n\n"
 
 
 	end := m.scroll + m.viewport
@@ -128,15 +191,16 @@ func (m InboxModel) View() string {
 		cursor := " "
 
 		if m.cursor == i {
-			cursor = ">"
+			cursor = cursorStyle.Render(">")
 		}
 
-		s += fmt.Sprintf("%s %s\n", cursor, m.emails[i])
+		s += fmt.Sprintf("%s %s - %s - %s\n", cursor, m.emails[i].Name, m.emails[i].Header, m.emails[i].From)
 
 	}
 
-	s += fmt.Sprintf("\n(Viewing %d-%d of %d)", m.scroll + 1, end, len(m.emails))
+	title := titleStyle.Render(" Inbox ")
+	box := boxStyle.Render(s)
 
-	return s
+	return fmt.Sprintf("%s\n%s\n", title, box)
 }
 
